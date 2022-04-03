41735e62934d37359e63240dbdd418199149a7a6
Rubylago <rubybibi6@gmail.com>
2022-04-03 13:23:45 +0800

    feat: add mongoose & dotenv

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const dotenv = require("dotenv");
+dotenv.config()
+
 const app = express()
 const port = 3000
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 app.get('/', (req, res) => {
   res.send('hi')
 })
