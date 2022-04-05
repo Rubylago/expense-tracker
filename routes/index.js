146e78bcccf83c9cfd15419708924ed38157efd6
Rubylago <rubybibi6@gmail.com>
2022-04-05 15:28:56 +0800

    feat: add user authentication in user, sort, expenses routers

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const home = require('./modules/home')
 const expenses = require('./modules/expenses')
 const sort = require('./modules/sort') 
 const users = require('./modules/user')
+const { authenticator  } = require('../middleware/auth')
 
 router.use('/users', users)
-router.use('/sort', sort)
-router.use('/expenses', expenses)
-router.use('/', home)
+router.use('/sort', authenticator, sort)
+router.use('/expenses', authenticator, expenses)
+router.use('/', authenticator, home)
 
 module.exports = router
